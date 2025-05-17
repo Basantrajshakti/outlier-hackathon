6b5558b61d5bf10886d68995242c71a5afe4ee47
Basantraj.shakti <basantrajshakti@gmail.com>
2025-05-17 15:04:55 +0530

    Preact initial setup: files cleaned

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+
+  return (
+    <>
+      <h1 class="text-5xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
